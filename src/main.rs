f59c3d04b78ea1cac91d6bb603cb8b24770e0bfc
zainsubhani <96117969+zainsubhani@users.noreply.github.com>
2025-01-09 17:29:07 -0500

    fileReaderRust

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use std::fs::File;
+use std::io::ErrorKind;
+use std::io::{BufRead, BufReader};
+
+fn main() {
+    let file = File::open("Cargo.toml");
+    let file = match file {
+        Ok(file) => file,
+        Err(error) => {
+            match error.kind() {
+                ErrorKind::NotFound => {
+                    println!("File not found: {}", error);
+                }
+                other_error => {
+                    println!("Problem opening the file: {:?}", other_error);
+                }
+            }
+            panic!("Problem opening the file: {:?}", error)
+        }
+    };
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+        match line {
+            Ok(line) => println!("{}", line),
+            Err(error) => {
+                panic!("Error reading line: {}", error)
+            }
+        }
+    }
+}
